f10224569ec2952a0d8b722897b8c43dfd36a527
Kyungdahm Yun <tomyun@gmail.com>
2015-05-18 18:47:46 -0700

    Clean up parameters for model running Support `export` flag

--- main.py
+++ main.py
@@ -285,8 +285,11 @@ def _slugify(v):
 def slugname(*args):
     return '_'.join([_slugify(k) for k in args])
 
-def run(weather_filename, weather_loc, observation_filename, observation_loc, species, cultivar, stage, calibrate_years, validate_years, export_years, n=3, MODELS=DEFAULT_MODELS):
+def run(weather, weather_loc, observation, observation_loc, cultivar, stage, calibrate_years, validate_years, export_years, n=3, MODELS=DEFAULT_MODELS, export=True):
+    weather_filename = 'data/{}.pkl'.format(weather)
     metdf = pd.read_pickle(weather_filename)
+
+    observation_filename = 'data/{}.pkl'.format(observation)
     obsdf = pd.read_pickle(observation_filename)
 
     def run_each(o, c):
@@ -308,7 +311,7 @@ def run(weather_filename, weather_loc, observation_filename, observation_loc, sp
         for m in models:
             #m.calibrate(calibrate_years)
             #multi.calibrate(m, calibrate_years)
-            multi.preset(slugname(m.name, w, o, species, c, stage, calibrate_years), m, calibrate_years, n)
+            multi.preset(slugname(m.name, w, o, observation, c, stage, calibrate_years), m, calibrate_years, n)
         # single model plot
         #plot_single_model(models, calibrate_years)
         #plot_single_model(models, calibrate_years, show_as_diff=True)
@@ -322,17 +325,19 @@ def run(weather_filename, weather_loc, observation_filename, observation_loc, sp
         e2.calibrate(calibrate_years)
 
         models = models + [e1, e2]
-
-        name = slugname(species, c, w, o, calibrate_years, stage)
-        name2 = slugname(species, c, w, o, calibrate_years, validate_years, stage)
-        show_single_summary(models, calibrate_years).to_csv('results/current/{}_calibrate.csv'.format(name))
-        show_single_summary(models, validate_years).to_csv('results/current/{}_validate.csv'.format(name2))
-        export_single_model(models, export_years).to_csv('results/current/{}_single.csv'.format(name))
-        export_multi_model(models, export_years).to_csv('results/current/{}_multi.csv'.format(name))
-        plot_single_model(models, calibrate_years, True, 'figures/current/{}_calibrate.png'.format(name))
-        plot_single_model(models, validate_years, True, 'figures/current/{}_validate.png'.format(name))
-        plot_single_model(models, export_years, True, 'figures/current/{}_export.png'.format(name))
-
+        if export:
+            name = slugname(observation, c, w, o, calibrate_years, stage)
+            name2 = slugname(observation, c, w, o, calibrate_years, validate_years, stage)
+            show_single_summary(models, calibrate_years).to_csv('results/current/{}_calibrate.csv'.format(name))
+            show_single_summary(models, validate_years).to_csv('results/current/{}_validate.csv'.format(name2))
+            export_single_model(models, export_years).to_csv('results/current/{}_single.csv'.format(name))
+            export_multi_model(models, export_years).to_csv('results/current/{}_multi.csv'.format(name))
+            plot_single_model(models, calibrate_years, False, 'figures/current/{}_calibrate_trend.png'.format(name))
+            plot_single_model(models, calibrate_years, True, 'figures/current/{}_calibrate_residual.png'.format(name))
+            plot_single_model(models, validate_years, False, 'figures/current/{}_validate_trend.png'.format(name))
+            plot_single_model(models, validate_years, True, 'figures/current/{}_validate_residual.png'.format(name))
+            plot_single_model(models, export_years, False, 'figures/current/{}_export_trend.png'.format(name))
+            plot_single_model(models, export_years, True, 'figures/current/{}_export_residual.png'.format(name))
         return models
 
     # populate available options from observation dataset
@@ -340,19 +345,18 @@ def run(weather_filename, weather_loc, observation_filename, observation_loc, sp
     cultivars = [cultivar] if cultivar else obsdf.index.levels[1]
     ocs = list(product(observations, cultivars))
     modelss = [run_each(*oc) for oc in ocs]
-
-    indices = ['_'.join([str(v) for v in oc]) for oc in ocs]
-    export_single_summaries(indices, modelss, calibrate_years, '{}_calibrate'.format(slugname(species, calibrate_years, stage)))
-    export_single_summaries(indices, modelss, validate_years, '{}_validate'.format(slugname(species, calibrate_years, validate_years, stage)))
+    if export:
+        indices = ['_'.join([str(v) for v in oc]) for oc in ocs]
+        export_single_summaries(indices, modelss, calibrate_years, '{}_calibrate'.format(slugname(observation, calibrate_years, stage)))
+        export_single_summaries(indices, modelss, validate_years, '{}_validate'.format(slugname(observation, calibrate_years, validate_years, stage)))
     return modelss
 
 def main2():
     # Cherry (DC)
-    weather_filename = 'data/dc.pkl'
+    weather = 'dc'
     weather_loc = 'USW00013743'
-    observation_filename = 'data/cherry_dc.pkl'
+    observation = 'cherry_dc'
     observation_loc = 'DC'
-    species = 'cherry'
     stage = 'Peak Bloom'
     export_years = (1946, 2015)
 
@@ -360,34 +364,33 @@ def main2():
     cultivar = 'Yoshino'
     calibrate_years = (1994, 2014)
     validate_years = (1946, 1993)
-    run(weather_filename, weather_loc, observation_filename, observation_loc, species, cultivar, stage, calibrate_years, validate_years, export_years, MODELS=DEFAULT_MODELS+[DegreeDay, February, March])
+    run(weather, weather_loc, observation, observation_loc, cultivar, stage, calibrate_years, validate_years, export_years, MODELS=DEFAULT_MODELS+[DegreeDay, February, March])
 
     # Cherry (DC) - Kwanzan
     cultivar = 'Kwanzan'
     calibrate_years = (1991, 2011)
     validate_years = (1946, 1990)
-    run(weather_filename, weather_loc, observation_filename, observation_loc, species, cultivar, stage, calibrate_years, validate_years, export_years, MODELS=DEFAULT_MODELS+[DegreeDay, February, March])
+    run(weather, weather_loc, observation, observation_loc, cultivar, stage, calibrate_years, validate_years, export_years, MODELS=DEFAULT_MODELS+[DegreeDay, February, March])
 
     # Apple
-    weather_filename = 'data/martinsburg.pkl'
+    weather = 'martinsburg'
     weather_loc = 'Martinsburg'
-    observation_filename = 'data/apple_kearneysville.pkl'
+    observation = 'apple_kearneysville'
     observation_loc = 'Kearneysville'
-    species = 'apple'
     cultivar = None
     stage = 'Full Bloom'
     calibrate_years = (1997, 2007)
     validate_years = calibrate_years
     export_years = (1950, 2010)
-    run(weather_filename, weather_loc, observation_filename, observation_loc, species, cultivar, stage, calibrate_years, validate_years, export_years)
+    run(weather, weather_loc, observation, observation_loc, cultivar, stage, calibrate_years, validate_years, export_years)
 
     calibrate_years = (2001, 2007)
     validate_years = (1997, 2000)
     export_years = (1950, 2010)
-    run(weather_filename, weather_loc, observation_filename, observation_loc, species, cultivar, stage, calibrate_years, validate_years, export_years)
+    run(weather, weather_loc, observation, observation_loc, cultivar, stage, calibrate_years, validate_years, export_years)
 
     # Korea (from Dr. Jina Hur)
-    weather_filename = 'data/korea_jina.pkl'
+    weather = 'korea_jina'
     weather_loc = None
     stage = 'FFD'
     calibrate_years = (1998, 2008)
@@ -395,31 +398,28 @@ def main2():
     export_years = (1982, 2010)
 
     # Peach (Korean)
-    observation_filename = 'data/peach_korea.pkl'
+    observation = 'peach_korea'
     observation_loc = None
-    species = 'peach'
     cultivar = 'Korean Peach'
-    run(weather_filename, weather_loc, observation_filename, observation_loc, species, cultivar, stage, calibrate_years, validate_years, export_years)
+    run(weather, weather_loc, observation, observation_loc, cultivar, stage, calibrate_years, validate_years, export_years)
 
     # Pear (Korean)
-    observation_filename = 'data/pear_korea.pkl'
+    observation = 'pear_korea'
     observation_loc = None
-    species = 'pear'
     cultivar = 'Korean Pear'
-    run(weather_filename, weather_loc, observation_filename, observation_loc, species, cultivar, stage, calibrate_years, validate_years, export_years)
+    run(weather, weather_loc, observation, observation_loc, cultivar, stage, calibrate_years, validate_years, export_years)
 
     # Cherry (Korean) (from Dr. Uran Chung)
-    weather_filename = 'data/korea_uran.pkl'
+    weather = 'korea_uran'
     weather_loc = None
-    observation_filename = 'data/cherry_korea.pkl'
+    observation = 'cherry_korea'
     observation_loc = None
-    species = 'cherry'
     cultivar = 'Korean Cherry'
     stage = 'Full Bloom'
     calibrate_years = (1984, 1994)
     validate_years = (1955, 1983)
     export_years = (1955, 2004)
-    run(weather_filename, weather_loc, observation_filename, observation_loc, species, cultivar, stage, calibrate_years, validate_years, export_years)
+    run(weather, weather_loc, observation, observation_loc, cultivar, stage, calibrate_years, validate_years, export_years)
 
 if __name__ == '__main__':
     main()
