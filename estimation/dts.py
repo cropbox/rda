353152d7259ddf40bd6c762a4e5b434b3e78d037
Kyungdahm Yun <tomyun@gmail.com>
2015-06-19 13:16:46 -0700

    Add standard temperature parameter Ts

--- estimation/dts.py
+++ estimation/dts.py
@@ -12,6 +12,7 @@ class Dts(Estimator):
     def coeff_names(self):
         return [
             'Ds', # start date (Julian)
+            'Ts', # standard temperature (C)
             'Ea', # temperature sensitivity rate (kJ mol-1)
             'Rd', # standard temperature accumulation requirement
         ]
@@ -22,16 +23,17 @@ class Dts(Estimator):
             #'coeff0': (20, 200),
             #'bounds': ((0, 100), (0, 500)),
             #'grid': (slice(10, 100, 1), slice(100, 300, 1)),
-            'coeff0': (1, 20, 200),
-            'bounds': ((-100, 100), (0, 200), (0, 500)),
-            'grid': (slice(-100, 100, 1), slice(0, 200, 1), slice(0, 500, 1)),
+            'coeff0': (1, 15, 20, 200),
+            'bounds': ((-100, 100), (-10, 30), (0, 200), (0, 500)),
+            'grid': (slice(-100, 100, 1), slice(-10, 30, 1), slice(0, 200, 1), slice(0, 500, 1)),
         }
 
     def _estimate(self, year, met, coeff):
         Ea = coeff['Ea']
         #T = met.tavg.resample('D') + 273.15
         T = met.tavg.resample('H') + 273.15
-        Ts = 271.4 # standard temperature (K)
+        #Ts = 271.4 # standard temperature (K)
+        Ts = coeff['Ts'] + 273.15
         R = 8.314 # gas constant (J K-1 mol-1)
         dts = np.exp(Ea * 1000. * (T - Ts) / (R * T * Ts))
         #dts = dts.resample('H', fill_method='ffill')
