67c7378ce3c4f1a8f52e47355545e12dbfa2196f
Kyungdahm Yun <tomyun@gmail.com>
2015-07-11 11:56:03 -0700

    Import seaborn for better graphics

--- model/group.py
+++ model/group.py
@@ -4,6 +4,7 @@ from .suite import ModelSuite, ModelSuiteError
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 from itertools import product
 from collections import OrderedDict
--- model/suite.py
+++ model/suite.py
@@ -5,6 +5,7 @@ from estimation import Ensemble
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 import json
 
