ae4496fc8b796f5ce72b47bd0557dc6a93436ea6
Kyungdahm Yun <tomyun@gmail.com>
2015-07-12 02:57:55 -0700

    Add Input class for input path management

--- util/path.py
+++ util/path.py
@@ -1,7 +1,7 @@
 import os
 
-class Output(object):
-    def __init__(self, basepath='./output', timestamp='current'):
+class Path(object):
+    def __init__(self, basepath, timestamp):
         self.basepath = os.path.abspath(os.path.expanduser(basepath))
         self.timestamp = timestamp
 
@@ -13,8 +13,21 @@ class Output(object):
             os.makedirs(p, exist_ok=True)
         return p
 
-    def filename(self, kind, basename, ext, timestamp=None, makedirs=True):
+    def filename(self, kind, basename, ext, timestamp=None, makedirs=False):
         p = self.path(kind, timestamp, makedirs)
         return os.path.join(p, '{}.{}'.format(basename, ext))
 
-output = Output()
+    def outfilename(self, kind, basename, ext, timestamp=None, makedirs=True):
+        return self.filename(kind, basename, ext, timestamp, makedirs)
+
+
+class Input(Path):
+    def __init__(self, basepath):
+        super(Input, self).__init__(basepath, timestamp='')
+input = Input(basepath='../input')
+
+
+class Output(Path):
+    def __init__(self, basepath, timestamp):
+        super(Output, self).__init__(basepath, timestamp)
+output = Output(basepath='../output', timestamp='current')
