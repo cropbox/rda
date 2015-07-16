190f1db4d9f74c73738fccde7f0bace51eafbd96
Kyungdahm Yun <tomyun@gmail.com>
2015-07-15 18:35:02 -0700

    Add conversion process for new datasets

--- code/pheno/util/data/conv.py
+++ code/pheno/util/data/conv.py
@@ -2,7 +2,9 @@ from .met import dc
 from .met import gunwi
 from .met import korea_jina
 from .met import korea_uran
+from .met import korea_shk060
 from .met import martinsburg
+from .met import usa_ds3505
 
 from .obs import apple_gunwi
 from .obs import apple_kearneysville
@@ -15,7 +17,9 @@ def conv():
     gunwi.conv()
     korea_jina.conv()
     korea_uran.conv()
+    korea_shk060.conv()
     martinsburg.conv()
+    usa_ds3505.conv()
 
     apple_gunwi.conv()
     apple_kearneysville.conv()
