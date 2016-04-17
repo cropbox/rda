773efde73bd4f72109fa80e7c1098fbb2d617b68
Kyungdahm Yun <tomyun@gmail.com>
2016-04-17 15:15:36 -0700

    Update cherry_dc_future for 20160308 output

--- code/cherry_dc_future.py
+++ code/cherry_dc_future.py
@@ -43,23 +43,32 @@ def plot_cherry_dc_future(df, selective=True, grouped=False, rolling=True, **kwa
         ax = sns.boxplot(data=mdf, x='interval', y='jday', hue='model')
     else:
         ax = sns.pointplot(data=mdf, x='year', y='jday', hue='model', linestyles=':', markers=markers, scale=0.7, **kwargs)
+        # for i, (k, g) in enumerate(mdf.groupby('model')):
+        #     plt.plot(g['year'], g['jday'], linestyle=':', marker=markers[i], label=k)
+        # ax = plt.legend(loc='best')
     return ax
 
 def plot_cherry_dc_future_all(output):
     cultivars = ['Yoshino', 'Kwanzan']
     scenarios = ['rcp45', 'rcp85']
     for c in cultivars:
+        if c == 'Yoshino':
+            ylim = (40, 100)
+        elif c == 'Kwanzan':
+            ylim = (70, 110)
         for s in scenarios:
             df = predict_cherry_dc_future(c, s, output)
 
-            plt.figure(figsize=(16,10))
-            plot_cherry_dc_future(df)
+            plt.figure(figsize=(15,8))
+            ax = plot_cherry_dc_future(df, grouped=True)
+            ax.set_ylim(ylim)
             plt.savefig(output.outfilename('results/{}'.format(c), '{}_{}_grouped'.format(c, s), 'png'))
 
-            plt.figure(figsize=(40,16))
-            plot_cherry_dc_future(df, grouped=False)
+            plt.figure(figsize=(15,6))
+            ax = plot_cherry_dc_future(df, grouped=False)
+            ax.set_ylim(ylim)
             plt.savefig(output.outfilename('results/{}'.format(c), '{}_{}_individual'.format(c, s), 'png'))
 
 if __name__ == '__main__':
-    output = Output(basepath='../output', timestamp='20160304-cherry-dc-future')
+    output = Output(basepath='../output', timestamp='20160308-cherry-dc-future')
     plot_cherry_dc_future_all(output)
